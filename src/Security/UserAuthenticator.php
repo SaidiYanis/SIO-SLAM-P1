3f008501d533a26d23fd395ebfad0a5b105b3367
Saidi Yanis <113110373+SaidiYanis@users.noreply.github.com>
2022-10-14 17:12:27 +0200

    Update UserAuthenticator.php

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
